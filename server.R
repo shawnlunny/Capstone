c9cd63b6e71a84c038a692c4619c47950d0a0fdf
Shawn Lunny <shawnlunny@gmail.com>
2018-10-21 19:22:10 -0500

    Updated algorithm to forward predict on the ngrams based on whats currently typed and what may be typed next

--- server.R
+++ server.R
@@ -20,34 +20,60 @@ shinyServer(function(input, output) {
     })
   
   nextWordPrediction <- function(text){
+    #get the word count
+    strcnt <- str_count(text, pattern="\\S+")
+    nxt <- 0
+    #check if there is a space at the end as the user is looking to type the next word and will want a prediction
+    if(str_sub(text,-1) == " "){
+      nxt <- 1
+    }
     
     #lookup word list in corpus data
-    if(text != ""){
-      #when using the unigram start searching on the last word in the string looking for one word matches
-      if(word(text,-1) != ""){
-        one <- grep(paste0("^",word(text,-1)),unigram$String, value=TRUE, perl=TRUE, useBytes=TRUE)
-      } else {
-        one <- grep(paste0("^",text),unigram$String, value=TRUE, perl=TRUE, useBytes=TRUE)
-      }
+    if(strcnt > 0){
       
-      #after every two words, go back one word and look for the next bigram to continue the string
-      if(str_count(text, pattern="\\S+") <= 2 && (word(text,-1) != "")){
-        two <- grep(paste0("^",text),bigram$String, value=TRUE, perl=TRUE, useBytes=TRUE)
-      } else {
-        #only show the user the last word of the bigram, since the first word was already typed in the string
-        two <- grep(paste0("^",word(text,-2,2)),bigram$String, value=TRUE, perl=TRUE, useBytes=TRUE)
-        two <- word(two,-1)
-      }
+      #check for space indicating user wants the next word otherwise return the current word being typed
+      unitext <- word(text, -1)
+      #will always return one word
+      one <- grep(paste0("^",unitext),unigram$String, value=TRUE, perl=TRUE, useBytes=TRUE)
+      
+      if(strcnt >= 3){
         
-      if(str_count(text, pattern="\\S+") <= 3 && (word(text,-1) != "")){
-        three <- grep(paste0("^",text),trigram$String, value=TRUE, perl=TRUE, useBytes=TRUE)
+        #check for space indicating user wants the next word prediction, otherwise return the current word pair being typed
+        bitext <- word(text, -2, strcnt)
+        two <- grep(paste0("^",bitext), bigram$String, value=TRUE, perl=TRUE, useBytes=TRUE)
+        #capture the last word of the next prediction
+        two <- word(two, -1)
+        
+        #look ahead for the new 3rd word
+        tritext <- word(text, -3, strcnt)
+        three <- grep(paste0("^",tritext), trigram$String, value=TRUE, perl=TRUE, useBytes=TRUE)
+        #capture the last word of the next prediction
+        three <- word(three, -1)
+        
+      } else if(strcnt == 2){
+        
+        #check for space indicating user wants the next word prediction, otherwise return the current word pair being typed
+        bitext <- word(text, -2, strcnt)
+        two <- grep(paste0("^",bitext),bigram$String, value=TRUE, perl=TRUE, useBytes=TRUE)
+        #result has 2 words but we want to show either the first or second depending on what the user has typed
+        two <- word(two, -1)
+        
+        three <- grep(paste0("^",text), trigram$String, value=TRUE, perl=TRUE, useBytes=TRUE)
+        #result has 3 words but we want to show either the first or second depending on what the user has typed
+        three <- word(three, -(strcnt-nxt))
+        
       } else {
-        #only show the user the last word of the trigram, since the first 2 words were already typed in the string
-        three <- grep(paste0("^",word(text,-3,3)),trigram$String, value=TRUE, perl=TRUE, useBytes=TRUE)
-        three <- word(three,-1)
+        
+        two <- grep(paste0("^",text), bigram$String, value=TRUE, perl=TRUE, useBytes=TRUE)
+        #result has 2 words but we want to show either the first or second depending on what the user has typed
+        two <- word(two, -(2-nxt))
+        
+        three <- grep(paste0("^",text), trigram$String, value=TRUE, perl=TRUE, useBytes=TRUE)
+        #result has 3 words but we want to show either the first or second depending on what the user has typed
+        three <- word(three, -(3-nxt))
       }
       
-      #choose the first (highest frequency) word set match from unigram, bigram, and trigram
+      #choose the highest frequency encountered word set match from unigram, bigram, and trigram data
       lookup <- data.frame(c(one[1], two[1], three[1]), stringsAsFactors = FALSE)
     }
   }
